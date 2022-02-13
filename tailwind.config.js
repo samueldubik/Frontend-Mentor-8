b3e1a790b9dc0f66291d5584a9a6d69c62a1d4af
Samuel Dubik <samuel.dubik@gmail.com>
2022-02-13 01:37:17 +0100

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+    },
+
+    colors: {
+      'orange': '#e38826',
+      'cyan' : '#006970',
+      'dark-cyan' : '#004241',
+      'white': '#ffffff',
+      'gray' : '#f2f2f2',
+    },
+
+    fontFamily: {
+      header: ['Big Shoulders Display'],
+      paragraph: ['Lexend Deca']
+    }
+  },
+  plugins: [],
+}
